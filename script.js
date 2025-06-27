3641bad89cdfff34170a567f528df39252df8276
Newton <newtonyt04@gamil.com>
2025-06-27 23:30:02 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const inputName = document.getElementById("inputName");
+const email = document.getElementById("email");
+const phone_number = document.getElementById("phoneNumber");
+const password = document.getElementById("password");
+const rePass = document.getElementById("rePassword");
+const submit_box = document.getElementById("submit_box");
+const allInput = document.querySelectorAll(".input_style");
+const form_box = document.getElementById("form_box");
+const nameValidator = /^[A-Za-z\s]+$/;
+
+const emailValidator = /^[\S]{3,13}(@gmail\.com)$/;
+const bdNUmberValidator = /^\+?[88]{0,2}-?01[3456789]\d{8}$/;
+const passValidator =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^*&]{8,}$/;
+
+console.log(passValidator.test("newtonA8#"));
+
+submit_box.addEventListener("click", (e) => {
+  e.preventDefault();
+  for (let element of allInput) {
+    if (!element.value.trim()) {
+      alert(element.name + " is blank");
+      return;
+    }
+  }
+
+  if (!nameValidator.test(inputName.value.trim())) {
+    alert("Name is Invalid");
+    return;
+  } else if (!bdNUmberValidator.test(phone_number.value.trim())) {
+    alert("Bd Phone Number Is Invalid");
+    return;
+  } else if (!emailValidator.test(email.value.trim())) {
+    alert("email is invalid");
+    return;
+  } else if (!passValidator.test(password.value.trim())) {
+    alert("Write a Strong Password");
+    return;
+  } else if (password.value.trim() !== rePass.value.trim()) {
+    alert("password not matched");
+    return;
+  }
+  form_box.submit();
+});
